ce4a8972d18ab46c1828e70067172c81f8baf0c9
Weston Verhulst <w2visme@gmail.com>
2018-04-30 16:16:11 -0500

    Added elevator functionality

--- cmake-build-debug/CMakeFiles/PrgAssignment3.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/PrgAssignment3.dir/DependInfo.cmake
@@ -4,9 +4,9 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/weston/Desktop/CS3050/PrgAssignment3/main.cpp" "/home/weston/Desktop/CS3050/PrgAssignment3/cmake-build-debug/CMakeFiles/PrgAssignment3.dir/main.cpp.o"
+  "/Users/westonmac/Desktop/ElevatorAlgo/main.cpp" "/Users/westonmac/Desktop/ElevatorAlgo/cmake-build-debug/CMakeFiles/PrgAssignment3.dir/main.cpp.o"
   )
-set(CMAKE_CXX_COMPILER_ID "GNU")
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
--- main.cpp
+++ main.cpp
@@ -1,35 +1,57 @@
 #include <iostream>
-#include <string>
-#include <stdio.h>
-#include <sstream>
-#include <stdlib.h>
-#include <unordered_map>
-#include <list>
-#include <string.h>
+#include <cstdlib>
+#include <vector>
+#include <queue>
+#include <cstdio>
+
 using namespace std;
 
+queue<int> Q;
+vector<int> visited(100000000);
+
 int main() {
-    cout<<"You have decided to take the elevator.\nEnter the total number of floors, the starting floor, goal floor, the upward floor increment, and the downward floor increment, in that order:\n";
+
+    cout<<"You are now in the elevator.\nEnter the total number of floors, the starting floor, goal floor, the upward floor increment, and the downward floor increment, in that order:\n";
     int totalFloors, goalFloor, startingFloor, upFloors, downFloors = 0;
 
-    //read in the input from command line
+            //read in the input from command line
     cin>> totalFloors>> startingFloor>>goalFloor>>upFloors>>downFloors;
 
-    if(startingFloor < 1){
-        cout<<"Your starting floor must be greater than zero.\n";
+
+    if (startingFloor == goalFloor) {
+        cout << "No need to take stairs or elevator. Already on goal floor.\n";
     }
-    if(startingFloor > totalFloors){
-        cout<<"Your starting floor cannot be greater than the total number of floors.\n";
+    //input is entered correctly
+    else{
+
+        visited[startingFloor] = 1;
+        Q.push(startingFloor);
+        while(!Q.empty()){
+            int currentFloor = Q.front();
+            Q.pop();
+
+            if((currentFloor+upFloors) <= totalFloors && visited[currentFloor+upFloors] ==0){
+                visited[currentFloor+upFloors] = visited[currentFloor] + 1;
+                Q.push(currentFloor+upFloors);
+            }
+
+            if((currentFloor-downFloors) >= 1 && visited[currentFloor-downFloors] == 0){
+                visited[currentFloor-downFloors] = visited[currentFloor+1];
+                Q.push(currentFloor-downFloors);
+            }
+        }
+
     }
-    if(totalFloors>100 || totalFloors<goalFloor){
-        cout<<"Your total number of floors must be less than 100 and greater than the goal floor.\n";
+
+    if(visited[goalFloor]!=0){
+        cout<<"\nThe minimum amount of button presses needed is..... "<<visited[goalFloor]-1<<"\n"<<"Thanks for taking the elevator!\n";
     }
-    if(upFloors<0){
-        cout<<"Your upward floor increment must greater than or equal to zero.\n";
+    else{
+        cout<<"\nSorry. You must use the stairs.\n";
     }
-    if(downFloors>100){
-        cout<<"Your downward floor increment must not exceed 100.\n";
-    }
+
+
+
 
     return 0;
 }
